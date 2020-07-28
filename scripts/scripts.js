ba306a43a478f013dfcf1a89532ba81f80d88dac
Jakub <jakub.mika94@gmail.com>
2020-07-29 00:15:42 +0100

    Add subpages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,16 @@
 var hamburger = document.querySelector(".hamburger");
 var nav = document.querySelector(".nav");
-var hamburgerIcon = document.querySelector(".hamburger_close")
-var hamburgerMenu = document.querySelector(".hamburger_menu")
+var hamburgerIcon = document.querySelector(".hamburger_close");
+var hamburgerMenu = document.querySelector(".hamburger_menu");
 
-hamburger.addEventListener("click", function(e){
-    if (nav.classList.contains("active")) {
-        nav.classList.remove("active")
-        hamburgerIcon.classList.remove("active")
-        hamburgerMenu.classList.remove("active")
-
-    } else {
-        nav.classList.add("active")
-        hamburgerIcon.classList.add("active")
-        hamburgerMenu.classList.add("active")
-        
-    }
-})
+hamburger.addEventListener("click", function (e) {
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+    hamburgerIcon.classList.remove("active");
+    hamburgerMenu.classList.remove("active");
+  } else {
+    nav.classList.add("active");
+    hamburgerIcon.classList.add("active");
+    hamburgerMenu.classList.add("active");
+  }
+});
